d6aea5da0d5b7e6d2380be73b30d83832f8942f4
chandan bag <cbag67612@gmail.com>
2025-05-03 17:46:27 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+  <title>Discuss Project</title>
+  <?php include("./client/commonFiles.php");?>
+</head>
+<body>
+
+
+
+
+
+  <?php include("./client/header.php")?>
+</body>
+</html>
